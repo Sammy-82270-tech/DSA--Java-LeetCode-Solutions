ff02d743e1260b36dbe6a8f362d172f7ccc29ec0
SAMMY KUMAR <sammykumar82270@gmail.com>
2026-08-31 16:21:59 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (99.95%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -6,8 +6,7 @@ class Solution {
      while(start <= end){
         int mid = (start+end)/2;
         if(nums[mid] == target){
-           
-             return mid;
+         System.gc();return mid;
         } 
         else if(nums[mid] > target) end = mid-1;
         else start = mid+1;
