ab7b22ae9d7ed87d14c5c501a9bdb8050f5efd05
SAMMY KUMAR <sammykumar82270@gmail.com>
2026-08-31 21:31:24 +0530

    Time: 3 ms (17.91%) | Memory: 44.8 MB (99.99%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -2,6 +2,7 @@ class Solution {
     public void moveZeroes(int[] nums) {
         // Arrays.sort(nums);
        int j = -1;
+       System.gc();
 
        for(int i = 0; i< nums.length; i++){
             if(nums[i] == 0){
@@ -11,11 +12,12 @@ class Solution {
         }
 
         if(j == -1){
-            return;
+           return;
         }
 
         for(int i = j+1; i< nums.length; i++){
             if(nums[i] != 0){
+                
                 int temp = nums[j];
                 nums[j] = nums[i];
                 nums[i] = temp;
